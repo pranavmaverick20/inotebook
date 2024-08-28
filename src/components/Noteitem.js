cb9013aa64b425b891a80ba33244c5c483611a3e
pranavmaverick20 <pranavahuja20@gmail.com>
2024-08-28 23:41:29 +0530

    added some more test notes

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -5,7 +5,7 @@ const Noteitem = (props) => {
     return (
 
         <div className="col-md-3">
-            <div class="card">
+            <div class="card my-2">
                 <div class="card-body">
                     <h5 class="card-title">{note.title}</h5>
                     <p class="card-text">{note.description}</p>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -7,7 +7,7 @@ const NoteState = (props) => {
         {
             "_id": "61322f19553781a8a8d006",
             "user": "6131d534037d4734a066",
-            "title": "My Title",
+            "title": "My Title 1",
             "description": "Please wake up early",
             "tag": "personal",
             "date": "2021-09-03T14:20:09.5092",
@@ -17,7 +17,57 @@ const NoteState = (props) => {
 
             "_id": "61322f19553781a8ca8d0e08",
             "user": "6131d53e4037cd4734a066",
-            "title": "My Title",
+            "title": "My Title 2",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2021-09-03T14:20:09.668Z",
+            "__v": 0
+        },
+        {
+
+            "_id": "61322f19553781a8ca8d0e08",
+            "user": "6131d53e4037cd4734a066",
+            "title": "My Title 3",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2021-09-03T14:20:09.668Z",
+            "__v": 0
+        },
+        {
+
+            "_id": "61322f19553781a8ca8d0e08",
+            "user": "6131d53e4037cd4734a066",
+            "title": "My Title 4",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2021-09-03T14:20:09.668Z",
+            "__v": 0
+        },
+        {
+
+            "_id": "61322f19553781a8ca8d0e08",
+            "user": "6131d53e4037cd4734a066",
+            "title": "My Title 5",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2021-09-03T14:20:09.668Z",
+            "__v": 0
+        },
+        {
+
+            "_id": "61322f19553781a8ca8d0e08",
+            "user": "6131d53e4037cd4734a066",
+            "title": "My Title 6",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2021-09-03T14:20:09.668Z",
+            "__v": 0
+        },
+        {
+
+            "_id": "61322f19553781a8ca8d0e08",
+            "user": "6131d53e4037cd4734a066",
+            "title": "My Title 7",
             "description": "Please wake up early",
             "tag": "personal",
             "date": "2021-09-03T14:20:09.668Z",
